3f670e80dbb6665d8a36958852a6c18df33b9134
GenesisMay <genesisdayanamayanzatonato@gmail.com>
2024-08-01 11:29:37 -0500

    eva

--- App.tsx
+++ App.tsx
@@ -4,10 +4,12 @@ import { Caja2 } from './screen/Caja2';
 import { SafeAreaView } from 'react-native';
 import { Caja3 } from './screen/Caja3';
 import { Caja4 } from './screen/Caja4';
+import { Numeros } from './screen/numeros';
 
 export const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
+    {/*<Numeros/>*/}
     {/*<CajasModelo1/>*/}
     {/*<Caja2/>*/}
     {/*<Caja3/>*/}
